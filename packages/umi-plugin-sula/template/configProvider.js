d171032049ae2dddc2e1a7001a15e4fd17903489
coolhub <xieyanxin93@gmail.com>
2021-08-27 19:22:10 +0800

    change to esm module import

--- packages/umi-plugin-sula/template/configProvider.js
+++ packages/umi-plugin-sula/template/configProvider.js
@@ -5,13 +5,18 @@ import { history } from 'umi';
 const baseSeparator = '{{{baseSeparator}}}' || '-';
 const formatLangFile = (lang) => lang && lang.replace(baseSeparator, '_');
 
+const supportLanguages = {
+  zh_CN: () => import('sula/es/localereceiver/zh_CN'),
+  en_US: () => import('sula/es/localereceiver/en_US'),
+};
+
 function getLocale() {
   const lang = '{{{default}}}' || `en${baseSeparator}US`;
   const langFile = formatLangFile(lang);
 
   let locale;
   try {
-    locale = require(`sula/es/localereceiver/${langFile}`);
+    locale = supportLanguages[langFile].then(l => l);
     locale = locale.default || locale;
   } catch (error) {}
   return locale;
